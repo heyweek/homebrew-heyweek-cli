70ab66c7caf392c33f08afa0b1ff81cdb1a1e695
goreleaserbot <hello@heyweek.com>
2025-04-19 11:44:07 +0200

    chore(release): brew formula update for Heyweek 1.4.48

--- heyweek-cli.rb
+++ heyweek-cli.rb
@@ -5,20 +5,20 @@
 class HeyweekCli < Formula
   desc "Heyweek CLI"
   homepage "https://heyweek.com"
-  version "1.4.47"
+  version "1.4.48"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/heyweek/homebrew-heyweek-cli/releases/download/v1.4.47/Heyweek_v1.4.47_macOS_amd64.zip", using: CurlDownloadStrategy
-    sha256 "c106840357ebafe4b9b9dbc2e3aedb171014cc2789840b79f85f1b3be328b9cd"
+    url "https://github.com/heyweek/homebrew-heyweek-cli/releases/download/v1.4.48/Heyweek_v1.4.48_macOS_amd64.zip", using: CurlDownloadStrategy
+    sha256 "0f42c520f096e52b6b4a6100c1b0661399a75eeeff80c0dc041586c4f1e50eeb"
 
     def install
       bin.install "bin/hw"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/heyweek/homebrew-heyweek-cli/releases/download/v1.4.47/Heyweek_v1.4.47_macOS_arm64.zip", using: CurlDownloadStrategy
-    sha256 "92f110a9db3043d5ad6902651e52b5a4727f3ac7f7d956fbc068b793ab0130f7"
+    url "https://github.com/heyweek/homebrew-heyweek-cli/releases/download/v1.4.48/Heyweek_v1.4.48_macOS_arm64.zip", using: CurlDownloadStrategy
+    sha256 "267912a22dec3eb00ea155b5082cb5487056815e252a3890cd9ce2bfacee6176"
 
     def install
       bin.install "hw"
